ca6d9aa77639a911a71b97aa8417d39b1c5f2de6
karendcl <karen.canterolopez@gmail.com>
2024-02-01 23:35:38 -0500

    Added the recommendation functionality using the distance between vectors

--- src/code/trial.py
+++ src/code/trial.py
@@ -1,131 +1,259 @@
 import re
 
+import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
-from sklearn.metrics import adjusted_rand_score
 import string
 from nltk.corpus import stopwords
-import json
 import glob
-import os
-import nltk
 from nltk.stem import WordNetLemmatizer
+import pickle
+from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
-nltk.download('wordnet')
+def load_docs() -> list:
+    """
+    Load the documents from the /Docs folder.
+    The documents are expected to be in .txt format
 
-#load documents from the Docs folder
-def load_docs():
+    Returns:
+        list: the list of documents
+    """
     data = []
+
+    #for file, replace with the cleaned text
     for file in glob.glob("Docs/*.txt"):
         with open(file, 'r') as f:
-            data.append(f.read())
+            data1 = f.read()
+            processed = clean_text(data1)
+            data.append(processed)
+        with open(file, 'w') as f:
+            f.write(processed)
+
     return data
 
-def remove_stops(text, stops):
+def remove_stops(text : string, stops :list) -> string:
+    """ Remove stop words and extra spaces from the text
+    
+    Parameters:
+        text (str): the text to be cleaned
+        stops (list): the list of stop words to be removed
+        
+    Returns:
+        str: the cleaned text
+    """
+    #remove stop words
     words = text.split()
     final = ' '.join([word for word in words if word not in stops])
     final = final.translate(str.maketrans('', '', string.punctuation))
 
+    #remove extra spaces
     while "  " in final:
         final = final.replace("  ", " ")
 
     return final
 
-def clean_docs(docs):
+def clean_text(text : string) -> string:
+    """ Clean the text by removing stop words and lemmatizing the words
+
+    Parameters:
+        text (str): the text to be cleaned
+
+    Returns:
+        str: the cleaned text
+    """
     stops = stopwords.words('english')
-    cleaned = [remove_stops(doc, stops) for doc in docs]
+    cleaned = remove_stops(text, stops)
 
     #lemmatize the words
     lemmatizer = WordNetLemmatizer()
-    for i in range(len(cleaned)):
-        words = cleaned[i].split()
-        lemmatized = [lemmatizer.lemmatize(word) for word in words]
-        cleaned[i] = ' '.join(lemmatized)
+    words = cleaned.split()
+    lemmatized = [lemmatizer.lemmatize(word) for word in words]
+    cleaned = ' '.join(lemmatized)
 
     return cleaned
 
 
-def tf_idf_and_k_means(documents):
-    vectorizer = TfidfVectorizer(stop_words='english',
-                                 lowercase=True,
-                                 ngram_range=(1, 3),
-                                 max_features=1000,
-                                 max_df=0.9,
-                                 smooth_idf=True,
-                                 min_df=1)
 
-    vectors = vectorizer.fit_transform(documents)
-    feature_names = vectorizer.get_feature_names_out()
-    dense = vectors.todense()
-    denselist = dense.tolist()
-
-    all_keywords = []
-    n_clusters_number = 4
-
-    for description in denselist:
-        x = 0
-        keywords = []
-        for i in description:
-            if i > 0:
-                keywords.append(feature_names[x])
-            x += 1
-        all_keywords.append(keywords)
-
-    model = KMeans(n_clusters=n_clusters_number, init='k-means++', max_iter=100, n_init=1)
-    model.fit(vectors)
-
-    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
-    terms = vectorizer.get_feature_names_out()
-
-    # load the results into json file
-    with open('results.json', 'w', encoding='utf-8') as f:
-        for i in range(n_clusters_number):
-            f.write("Cluster %d:" % i)
-            for ind in order_centroids[i, :10]:
-                f.write(' %s' % terms[ind])
-                f.write("\n")
-            f.write("\n\n")
-
-
-from nltk.tokenize import RegexpTokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import LatentDirichletAllocation
 def lda_model(documents, num_topics=4):
+    """ Perform Latent Dirichlet Allocation on the documents
+    Save the model and the count vectorizer to be used later with new data
+
+Parameters:
+    documents (list): the list of documents to be analyzed
+    num_topics (int): the number of topics to be found
+
+Returns:
+    None
+
+    """
+
     tokenizer = RegexpTokenizer(r'\w+')
 
-    tfidf = TfidfVectorizer(tokenizer=tokenizer.tokenize, stop_words='english', lowercase=True, ngram_range=(1,3))
-    train_data = tfidf.fit_transform(documents)
+    count_vectorizer = TfidfVectorizer(tokenizer=tokenizer.tokenize, stop_words='english', lowercase=True, ngram_range=(1,3))
 
-    model = LatentDirichletAllocation(n_components=num_topics)
+    train_data = count_vectorizer.fit_transform(documents)
+
+    model = LatentDirichletAllocation(n_components=num_topics, learning_method='online')
     lda_matrix = model.fit_transform(train_data)
     lda_components = model.components_
 
-    terms = tfidf.get_feature_names_out()
+    terms = count_vectorizer.get_feature_names_out()
 
-    for topic_idx, topic in enumerate(lda_components):
-        zippped = zip(terms, topic)
-        sorted_terms = sorted(zippped, key=lambda x:x[1], reverse=True)
-        print("Topic %d:" % topic_idx)
-        for i in range(10):
-            print(sorted_terms[i])
+    #print the topics
+    for i, topic in enumerate(lda_components):
+        print(f"Top words for topic {i}\n")
+        print([terms[i] for i in topic.argsort()[-10:]])
         print("\n")
 
+    #save lda model to file to be retrained later
+    with open('lda_model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    #save the count vectorizer to file to be retrained later
+    with open('count_vectorizer.pkl', 'wb') as f:
+        pickle.dump(count_vectorizer, f)
+
+    with open('lda_matrix.pkl', 'wb') as f:
+        pickle.dump(lda_matrix, f)
 
     #build document-topic matrix
     doc_topic = pd.DataFrame(lda_matrix, columns=["Topic "+str(i) for i in range(num_topics)], index=["Doc "+str(i) for i in range(len(documents))])
     print(doc_topic)
 
+
 def Amount_of_topics(documents):
-    return len(documents)//2 +1
+    """
+    Calculate the amount of topics to be used in the LDA model
 
-#load the documents
-descriptions = load_docs()
-cleaned_data = clean_docs(descriptions)
-print(cleaned_data)
+    Parameters:
+        documents (list): the list of documents
 
-lda_model(cleaned_data, Amount_of_topics(cleaned_data))
+    Returns:
+        int: the amount of topics to be used
+    """
+    return len(documents)//2 + 1
 
-#tf_idf_and_k_means(cleaned_data)
+
+def Add_New_Document(text, title):
+    """
+    Fits the new document to the data without retraining it.
+    Saves the new document to the Docs folder
+
+    Parameters:
+        alldocs (list): the list of all documents
+        text (str): the text of the new document
+        title (str): the title of the new document
+
+    Returns:
+        None
+
+    """
+
+    cleaned_text = [clean_text(text)]
+
+    #count how many documents there are
+    count = 0
+    for file in glob.glob("Docs/*.txt"):
+        count += 1
+
+    #save it to a txt
+    with open(f'Docs/{title}.txt', 'w') as f:
+        f.write(cleaned_text[0])
+
+    #see if the quantity of documents changed
+    new_count = 0
+    for file in glob.glob("Docs/*.txt"):
+        new_count += 1
+
+    if new_count > count:
+        with open('lda_model.pkl', 'rb') as f:
+            model = pickle.load(f)
+
+        with open('count_vectorizer.pkl', 'rb') as f:
+            count_vectorizer = pickle.load(f)
+
+        new_data = count_vectorizer.transform(cleaned_text)
+
+        old_matrix = model.components_
+
+        lda_matrix = model.partial_fit(new_data)
+
+        new_data = new_data.reshape(1, -1)
+        topic_dist = lda_matrix.transform(new_data)
+
+
+        #save the new model and count vectorizer
+        with open('lda_model.pkl', 'wb') as f:
+            pickle.dump(lda_matrix, f)
+
+        with open('count_vectorizer.pkl', 'wb') as f:
+            pickle.dump(count_vectorizer, f)
+
+        with open('lda_matrix.pkl', 'wb') as f:
+            pickle.dump(topic_dist, f)
+
+        # insert the new document into the document-topic matrix
+        doc_topic = pd.DataFrame(lda_matrix, columns=["Topic " + str(i) for i in range(len(old_matrix))],
+                                     index=["Doc " + str(i) for i in range(new_count)])
+        return doc_topic
+    else:
+        return "Document Already Exists"
+
+
+def Load_First_Time():
+    """
+    Load the documents for the first time and train the LDA model
+
+    Returns:
+        list: the list of documents
+    """
+    descriptions = load_docs()
+    lda_model(descriptions, Amount_of_topics(descriptions))
+    return descriptions
+
+def make_suggestion(docs_read):
+    """
+    Make a suggestion of the most similar documents to the ones read
+
+    Parameters:
+        docs_read (list): the list of indices of the documents read
+
+    Returns:
+        list: the list of indices of the most similar documents ordered by similarity
+
+
+    """
+
+    #load model
+    with open('lda_matrix.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    count = len(model)
+
+    mask = [True if i in docs_read else False for i in range(count)]
+
+    print(mask)
+
+    docs_read = model[mask]
+
+    docs_not_read_ind = [i for i in range(count) if mask[i] == False]
+
+    #get avg of docs read
+    avg_read = np.mean(docs_read, axis=0)
+
+    #order the docs not read by similarity to the avg of docs read
+    def similarity(x):
+        return np.dot(avg_read, x)/(np.linalg.norm(avg_read)*np.linalg.norm(x))
+
+    dict = {}
+    for i in docs_not_read_ind:
+        dict[i] = similarity(model[i])
+
+    docs_not_read = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+
+
+    #return indices of the most similar docs
+    return [i[0] for i in docs_not_read]
 
 
